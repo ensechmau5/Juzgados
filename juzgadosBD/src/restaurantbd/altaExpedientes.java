5719e615994dbe2ac12081b3bf56210d8a640a04
Juan Daniel Martínez <mtzn_juandaniel@yahoo.com.mx>
2018-06-04 15:11:21 -0600

    Update altaExpedientes.java

--- juzgadosBD/src/restaurantbd/altaExpedientes.java
+++ juzgadosBD/src/restaurantbd/altaExpedientes.java
@@ -10,6 +10,7 @@ public class altaExpedientes {
     private int id;
     private String fecha;
     private int tipo;
+    private String estado;
    
     
     
@@ -19,10 +20,11 @@ public class altaExpedientes {
      * @param clave_ingred clave del ingrediente
      * @param nombre Nombre del Ingrediente
      */
-    public altaExpedientes(int id, String fecha, int tipo) {
+    public altaExpedientes(int id, String fecha, int tipo, String estado) {
         this.id = id;
         this.fecha = fecha;
         this.tipo = tipo;
+        this.estado = estado;
         
     }
     
@@ -54,7 +56,9 @@ public class altaExpedientes {
        return tipo;
    }
    
-   
+   public String getEstado() {
+       return estado;
+   }
    
     @Override
     public String toString() {
